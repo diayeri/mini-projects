32ce9b59b1cdb053e1948a71e2e3dc99c63ec3ee
dia <jdyoung1031@gmail.com>
2024-07-25 15:30:31 +0900

    전체구조 먼저 만들기

--- todo/index.js
+++ todo/index.js
@@ -16,6 +16,7 @@ const addListUi = (text) => {
   $btnEdit.textContent = "수정";
 
   const $check = document.createElement("input");
+  $check.classList.add("checkbox");
   $check.setAttribute("type", "checkbox");
 
   $label.textContent = text.todo;
@@ -23,6 +24,7 @@ const addListUi = (text) => {
   $label.prepend($check);
 };
 
+// 불러오기
 const fetchTodos = async () => {
   const data = await fetch("http://localhost:3000/todos");
   const todos = await data.json();
@@ -33,8 +35,8 @@ const fetchTodos = async () => {
 };
 fetchTodos();
 
-// 입력하기 통신
-const addTodo = async (todoText) => {
+// 추가하기
+const addList = async (todoText) => {
   try {
     const req = await fetch("http://localhost:3000/todos", {
       method: "POST",
@@ -51,13 +53,32 @@ const addTodo = async (todoText) => {
   } catch (error) {
     console.log(error);
   } finally {
-    console.log("Todos added ended, anyway");
+    console.log("Adding list ended, anyway");
   }
 };
 
+// form - submit event
 $form.addEventListener("submit", (e) => {
   e.preventDefault();
   const todoText = $addInput.value;
-  addTodo(todoText);
+  addList(todoText);
   $addInput.value = "";
+});
+
+// 완료하기
+const checkList = async (todoText) => {};
+// 삭제하기
+const delList = async (todoText) => {};
+// 수정하기
+const editList = async (todoText) => {};
+
+// list - button, input event
+$ol.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-del")) {
+    console.log("del", e.target);
+  } else if (e.target.classList.contains("checkbox")) {
+    console.log("check", e.target);
+  } else if (e.target.classList.contains("btn-edit")) {
+    console.log("edit", e.target);
+  }
 });
