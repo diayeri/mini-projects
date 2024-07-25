989eb790b1a8d36a043218aa197423de0dd599c4
dia <jdyoung1031@gmail.com>
2024-07-25 19:48:03 +0900

    완료체크 기능추가

--- todo/index.js
+++ todo/index.js
@@ -9,7 +9,9 @@ const $form = document.querySelector("form");
 const addListUi = (list) => {
   const $li = document.createElement("li");
   $li.id = list.id;
+
   const $label = document.createElement("label");
+  $label.setAttribute("for", `checkbox-${list.id}`);
 
   const $btnDel = document.createElement("button");
   $btnDel.classList.add("btn-del");
@@ -20,12 +22,12 @@ const addListUi = (list) => {
   $btnEdit.textContent = "수정";
 
   const $check = document.createElement("input");
-  $check.classList.add("checkbox");
   $check.setAttribute("type", "checkbox");
+  $check.id = `checkbox-${list.id}`;
+  $check.checked = list.done; // 체크상태 불러오기
 
   $label.textContent = list.todo;
-  $ol.appendChild($li).append($label, $btnDel, $btnEdit);
-  $label.prepend($check);
+  $ol.appendChild($li).append($check, $label, $btnDel, $btnEdit);
 };
 
 // 불러오기
@@ -53,7 +55,7 @@ const addList = async (todoText) => {
     const newList = await req.json(); // 새로 추가된 요소
     addListUi(newList);
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 };
 
@@ -76,18 +78,37 @@ const delList = async (id) => {
     });
     res.ok ? true : false;
   } catch (error) {
-    console.log("ERR", error);
+    console.error(error);
   }
 };
 
 // 완료하기
-const checkList = async (text) => {};
+// 체크박스 상태가 바뀌는 것을 감지해야함
+// 상태가 바뀌면 서버 데이터 변경을 요청한다
+// 데이터가 잘 변경되었는지 확인한다
+// 데이터에 체크박스의 ui를 연결한다(?)
+const checkList = async (id, checked) => {
+  try {
+    const res = await fetch(url + "/" + id, {
+      method: "PATCH",
+      headers: fetchHeaders,
+      body: JSON.stringify({
+        done: checked,
+      }),
+    });
+    const body = await res.json();
+    return body.done; // resolve 된거랑 동일!!
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 // 수정하기
 const editList = async (text) => {};
 
 // list - button, input event
-$ol.addEventListener("click", (e) => {
+$ol.addEventListener("click", async (e) => {
+  // console.log(e.target);
   if (e.target.classList.contains("btn-del")) {
     if (confirm("정말 삭제할까요?")) {
       const parentNode = e.target.parentNode;
@@ -97,9 +118,16 @@ $ol.addEventListener("click", (e) => {
     } else {
       console.log("삭제가 취소되었습니다");
     }
-  } else if (e.target.classList.contains("checkbox")) {
-    console.log("check", e.target);
+  } else if (e.target.tagName === "INPUT") {
+    const listId = e.target.closest("li").id;
+    const checked = e.target.checked;
+    // console.log(listId, checked);
+    e.target.checked = await checkList(listId, checked); // 서버에서 변경된 값 받아오기
+    const state = await checkList(listId, checked);
+    console.log("체크박스 상태 변경", e.target.checked);
+    console.log("체크박스 상태 변경", state);
   } else if (e.target.classList.contains("btn-edit")) {
     console.log("edit", e.target);
   }
 });
+// checkbox input onchange
