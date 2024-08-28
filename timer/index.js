68c0908401db68f7ad8727544740e83e38205a00
dia <jdyoung1031@gmail.com>
2024-08-28 15:32:49 +0900

    기능 기본구조

--- timer/index.js
+++ timer/index.js
@@ -1,1 +1,36 @@
-console.log("hi");
+const $btns = document.querySelector("#btns");
+const $startBtn = document.querySelector("#startBtn");
+const $pauseBtn = document.querySelector("#pauseBtn");
+const $resetBtn = document.querySelector("#resetBtn");
+
+$btns.addEventListener("click", (e) => {
+  if (e.target.id === "startBtn") {
+    startTimer();
+  }
+  if (e.target.id === "pauseBtn") {
+    pauseTimer();
+  }
+  if (e.target.id === "resetBtn") {
+    resetTimer();
+  }
+});
+
+// 시계 기능
+// 1. 60초가 1분, 60분이 1시간
+// 2. 1초씩 시간 줄어들기
+const timer = () => {};
+
+// 타이머 시작하기
+const startTimer = () => {
+  // $startBtn.disabled = true;
+  $startBtn.classList.add("hidden");
+  $pauseBtn.classList.remove("hidden");
+};
+
+// 타이머 멈추기
+const pauseTimer = () => {};
+
+// 타이머 초기화
+// 1. 0초가 되면 실행
+// 2. reset 버튼 누르면 실행
+const resetTimer = () => {};
