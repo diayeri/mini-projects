a9a69f72ace97c079048a87e09dbaa55249b73e5
dia <jdyoung1031@gmail.com>
2024-08-28 15:48:31 +0900

    초기화 기본기능

--- timer/index.js
+++ timer/index.js
@@ -3,6 +3,9 @@ const $startBtn = document.querySelector("#startBtn");
 const $pauseBtn = document.querySelector("#pauseBtn");
 const $resetBtn = document.querySelector("#resetBtn");
 
+const $input = document.querySelectorAll(".time");
+const $hour = document.querySelector("#hour");
+const $min = document.querySelector("#min");
 const $sec = document.querySelector("#sec");
 
 $btns.addEventListener("click", (e) => {
@@ -22,28 +25,34 @@ $btns.addEventListener("click", (e) => {
 // 2. 60초가 1분, 60분이 1시간
 const timer = () => {};
 
-const timerId = setInterval(() => {
-  let timeLeft = $sec.value;
-
-  if ($sec.value <= 0) {
-    return clearInterval(timerId);
-  }
-
-  timeLeft -= 1;
-  $sec.value = String(timeLeft).padStart(2, "0");
-}, 1000);
-
 // 타이머 시작하기
 const startTimer = () => {
   // $startBtn.disabled = true;
   $startBtn.classList.add("hidden");
   $pauseBtn.classList.remove("hidden");
+
+  const timerId = setInterval(() => {
+    let timeLeft = $sec.value;
+
+    if ($sec.value <= 0) {
+      return clearInterval(timerId);
+    }
+
+    timeLeft -= 1;
+    $sec.value = String(timeLeft).padStart(2, "0");
+  }, 200);
 };
 
 // 타이머 멈추기
 const pauseTimer = () => {};
 
 // 타이머 초기화
-// 1. 0초가 되면 실행
-// 2. reset 버튼 누르면 실행
-const resetTimer = () => {};
+// 1. 0초가 되면 초기화 실행
+// 2. reset 버튼 누르면 초기화 실행
+const resetTimer = () => {
+  $input.forEach((i) => {
+    i.value = "00";
+  });
+  $pauseBtn.classList.add("hidden");
+  $startBtn.classList.remove("hidden");
+};
